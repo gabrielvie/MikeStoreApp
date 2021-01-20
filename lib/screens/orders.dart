ed96b93951067a92aa8df1d1bdb7eae945793ba3
Gabriel Vieira <gabrielvie@users.noreply.github.com>
2021-01-19 22:23:53 -0300

    feat(order): create a new order screen.

---
+++ lib/screens/orders.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:shopapp/providers/orders.dart' show OrdersProvider;
+import 'package:shopapp/widgets/order_item.dart';
+
+class OrdersScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final ordersProvider = Provider.of<OrdersProvider>(context);
+    return Scaffold(
+      appBar: AppBar(title: const Text('Your Orders!')),
+      body: ListView.builder(
+        itemBuilder: (_, index) => OrderItem(
+          order: ordersProvider.items[index],
+        ),
+        itemCount: ordersProvider.items.length,
+      ),
+    );
+  }
+}
