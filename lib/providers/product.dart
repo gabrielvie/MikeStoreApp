4588bf13757a75bbdf1e1e242ea0058bafe6bb30
Gabriel Vieira <gabrielvie@gmail.com>
2021-02-14 16:02:28 -0300

    feat(product): adding methods to help convert Product class to json string and json string to product class.

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 
 class Product with ChangeNotifier {
@@ -20,5 +22,57 @@ class Product with ChangeNotifier {
   void toogleFavoriteStatus() {
     isDesired = !isDesired;
     notifyListeners();
+  }
+
+  Product copyWith({
+    String id,
+    String title,
+    String imageUrl,
+    String description,
+    double price,
+    bool isDesired,
+  }) {
+    return Product(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      imageUrl: imageUrl ?? this.imageUrl,
+      description: description ?? this.description,
+      price: price ?? this.price,
+      isDesired: isDesired ?? this.isDesired,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'imageUrl': imageUrl,
+      'description': description,
+      'price': price,
+      'isDesired': isDesired,
+    };
+  }
+
+  factory Product.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return Product(
+      id: map['id'],
+      title: map['title'],
+      imageUrl: map['imageUrl'],
+      description: map['description'],
+      price: map['price'],
+      isDesired: map['isDesired'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Product.fromJson(String source) =>
+      Product.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Product(id: $id, title: $title, imageUrl: $imageUrl, description: $description, price: $price, isDesired: $isDesired)';
   }
 }
